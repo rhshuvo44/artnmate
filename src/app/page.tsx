8790a0e688b94fe4210dd3a8ca8704d5df6e19a8
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2026-01-27 13:54:23 +0600

    fact:Home pages fun fact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FunFact from "@/component/home/FunFact";
 import Hero from "@/component/home/Hero";
 import homeData from "@/DB/home.json";
 export default function Home() {
@@ -15,6 +16,16 @@ export default function Home() {
         bgImageUrl="/images/hero_bg.jpeg"
       />
       {/* End Hero Section */}
+       {/* Start FunFact Section */}
+      <div className="container">
+        <FunFact
+          variant="cs-type1"
+          title="Our fun fact"
+          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
+          data={homeData?.funfaceData}
+        />
+      </div>
+      {/* End FunFact Section */}
     </>
   );
 }
