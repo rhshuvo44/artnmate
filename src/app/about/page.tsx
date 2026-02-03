e9fc4bc2da8929a672de3e61db44ac1f155b94f6
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2026-02-03 21:33:57 +0600

    Fact: About  page about section and banner

---
+++ src/app/about/page.tsx
@@ -0,0 +1,80 @@
+import PageHeading from "@/component/about/PageHeading";
+import Div from "@/component/ui/Div";
+import SectionHeading from "@/component/ui/SectionHeading";
+import Spacing from "@/component/ui/Spacing";
+import Image from "next/image";
+// import aboutImg from "./images/about_img_1.jpeg";
+// import aboutImg2 from "./images/about_img_2.jpeg";
+// import aboutImg3 from "./images/about_img_3.jpeg";
+// import aboutImg4 from './images/about_img_4.jpeg'
+const About = () => {
+  return (
+    <>
+      {/* Start Page Heading Section */}
+      <PageHeading
+        title="About Us"
+        bgSrc="/images/about_hero_bg.jpeg"
+        pageLinkText="About Us"
+      />
+      {/* End Page Heading Section */}
+      {/* Start About Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="container">
+        <Div className="row">
+          <Div className="col-xl-5 col-lg-7">
+            <SectionHeading
+              title="Your trusted partner for business"
+              subtitle="About Our Agency"
+            >
+              <Spacing lg="30" md="20" />
+              <p className="cs-m0">
+                This is the main factor that sets us apart from our competition
+                and allows us to deliver a specialist business consultancy
+                service. Our team applies its wide-ranging experience to
+                determining. Through our years of experience, we’ve also learned
+                that while each channel.
+              </p>
+              <Spacing lg="30" md="30" />
+              <Div className="cs-separator cs-accent_bg"></Div>
+              <Spacing lg="25" md="40" />
+            </SectionHeading>
+          </Div>
+          <Div className="col-lg-5 offset-xl-2">
+            <Image
+              src="/images/about_img_1.jpeg"
+              alt="About"
+              width={600}
+              height={420}
+              className="w-100 cs-radius_15"
+            />
+            <Spacing lg="25" md="25" />
+          </Div>
+          <Div className="col-lg-7">
+            <Image
+              width={600}
+              height={420}
+              src="/images/about_img_2.jpeg"
+              alt="About"
+              className="w-100 cs-radius_15"
+            />
+            <Spacing lg="25" md="25" />
+          </Div>
+          <Div className="col-lg-5">
+            <Image
+              width={600}
+              height={420}
+              src="/images/about_img_3.jpeg"
+              alt="About"
+              className="w-100 cs-radius_15"
+            />
+            <Spacing lg="25" md="25" />
+          </Div>
+        </Div>
+      </Div>
+      <Spacing lg="75" md="55" />
+      {/* End About Section */}
+    </>
+  );
+};
+
+export default About;
